6f3797d896382611faaf8d58c4777d1e798fbf9a
siavash <siamhb@protonmail.com>
2022-07-07 19:43:53 +0430

    build a base of the project

---
+++ src/Styles/darkTheme.tsx
@@ -0,0 +1,33 @@
+import { createTheme } from "@stitches/react";
+
+
+const darkTheme = createTheme("dark-theme" , {
+    colors:{
+        "primary":"rgba(0,105,110,1)",
+        "primary100":"rgba(0,105,110,0.1)",
+        "primary200":"rgba(0,105,110,0.2)",
+        "primary300":"rgba(0,105,110,0.3)",
+        "primary400":"rgba(0,105,110,0.4)",
+        "primary500":"rgba(0,105,110,0.5)",
+        "primary600":"rgba(0,105,110,0.6)", 
+        "primary700":"rgba(0,105,110,0.7)",
+        "primary800":"rgba(0,105,110,0.8)",
+        "primary900":"rgba(0,105,110,0.9)",
+        "onPrimary":"#FFFFFF",
+        "bg":"#1A1C18",
+        "onBg":"#E2E3DC",
+        "onBg100":"rgba(226,227,220,0.1)",
+        "onBg200":"rgba(226,227,220,0.2)",
+        "onBg300":"rgba(226,227,220,0.3)",
+        "onBg400":"rgba(226,227,220,0.4)",
+        "onBg500":"rgba(226,227,220,0.5)",
+        "onBg600":"rgba(226,227,220,0.6)",
+        "onBg700":"rgba(226,227,220,0.7)",
+        "onBg800":"rgba(226,227,220,0.8)",
+        "onBg900":"rgba(226,227,220,0.9)",
+        
+    },
+})
+
+
+export default darkTheme
